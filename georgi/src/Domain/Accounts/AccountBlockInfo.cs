b23688b91f2af5af79d5ebc17a2c6b0208db322b
Gocata <58916152+georgy-kirilov@users.noreply.github.com>
2025-02-24 17:50:42 +0200

    Code refactor

--- georgi/src/Domain/Accounts/AccountBlockInfo.cs
+++ georgi/src/Domain/Accounts/AccountBlockInfo.cs
@@ -2,7 +2,7 @@ namespace Domain.Accounts;
 
 public sealed class AccountBlockInfo
 {
-    public required bool HasPendingBlocks { get; init; }
-
     private AccountBlockInfo() { }
+
+    public required bool HasPendingBlocks { get; init; }
 }
--- georgi/src/Domain/Accounts/AccountDomainException.cs
+++ georgi/src/Domain/Accounts/AccountDomainException.cs
@@ -1,6 +1,7 @@
 namespace Domain.Accounts;
 
-public sealed class AccountDomainException(AccountId accountId, string reason) : Exception
+public sealed class AccountDomainException(AccountId accountId, string reason)
+    : Exception($"An unexpected error for card with id '{accountId.Value}' has occurred: {reason}.")
 {
     public AccountId AccountId { get; } = accountId;
 
